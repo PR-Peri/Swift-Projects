58048e2d64d26d18ab760bdc3118ee85eb853c15
perivitta <46223444+Perivitta@users.noreply.github.com>
2022-12-11 21:42:46 +0800

    Transfered two new repos

---
+++ Nike-iOS-Application/Peroush Nike App/Peroush_Nike_AppApp.swift
@@ -0,0 +1,18 @@
+//
+//  Peroush_Nike_AppApp.swift
+//  Peroush Nike App
+//
+//  Created by Perivitta Rajendran on 02/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct Peroush_Nike_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
